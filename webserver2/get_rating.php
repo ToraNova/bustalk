573a575e9d88b1ba80b6367daebf2df5c02af39d
MaHooo13 <42143209+MaHooo13@users.noreply.github.com>
2018-09-27 15:45:18 +0800

    Update get_rating.php

--- webserver2/get_rating.php
+++ webserver2/get_rating.php
@@ -1,17 +1,17 @@
 <?php
 $dbName="bustalk";
-$con=mysqli_connect("localhost","root","");
-	if (mysqli_connect_errno()){
+$con=mysqli_connect("localhost","root","");								//Establish connection with server
+	if (mysqli_connect_errno()){									//If cannot connect to database display error
 		echo ('9.9'); //error
 		//echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
-	if(!mysqli_select_db($con,$dbName)){
+	if(!mysqli_select_db($con,$dbName)){								//If database not found display error 
 		exit('<p>Database not found!:(</p>');
 	}else{
 
 	}
 ?>
-<?php
+<?php													//Get driver rating from server through driver ID
 	if(isset($_GET['drvid'])){
 		$drvid = $_GET['drvid'];
 		$queryString = "select avg(bus_rating) as rating from bus_rating join driver on bus_rating.bus_id = driver.bus_id
@@ -19,6 +19,6 @@ $con=mysqli_connect("localhost","root","");
 		$query_result = mysqli_query($con,$queryString);
 		echo mysqli_fetch_array($query_result)['rating'];
 	}
-	mysqli_close($con);
+	mysqli_close($con);										//Close connection with server
 	//http://localhost/get_rating.php?drvid=1234567890
 ?>
